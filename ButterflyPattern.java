4b96ab7b3618bc693401ae42a9791c9995a312d7
ThAditya <Aditya.vashishth95@gmail.com>
2024-10-23 00:51:34 +0530

    butterfly

---
+++ ButterflyPattern.java
@@ -0,0 +1,40 @@
+
+import java.util.*;
+
+public class ButterflyPattern {
+
+    public static void flyPattern(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int k = 1; k <= 2 * (n - i); k++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+        for (int i = n; i >= 1; i--) {
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int k = 1; k <= 2 * (n - i); k++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        flyPattern(n);
+    }
+}
