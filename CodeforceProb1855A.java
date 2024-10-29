5321ea3ca6fe9f81f512e44fe6e779b379b87ae9
ThAditya <Aditya.vashishth95@gmail.com>
2024-10-29 23:43:06 +0530

    Codeforce problem

---
+++ CodeforceProb1855A.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class CodeforceProb1855A {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- >= 1) {
+            int n = sc.nextInt();
+            int seats[] = new int[n + 1];
+            for (int i = 1; i <= n; i++) {
+                seats[i] = sc.nextInt();
+            }
+            double count = 0;
+            for (int i = 1; i <= n; i++) {
+                if (i == seats[i]) {
+                    count++;
+                }
+            }
+            System.out.println((int) Math.ceil(count / 2));
+        }
+    }
+}
