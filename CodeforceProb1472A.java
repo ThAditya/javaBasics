80fcd11b6d787a88aca7ae548f8a7c0178443d96
ThAditya <Aditya.vashishth95@gmail.com>
2024-10-30 23:54:21 +0530

    new codeforce prob

---
+++ CodeforceProb1472A.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+
+public class Problem1472A {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- >= 1) {
+            int w = sc.nextInt();
+            int h = sc.nextInt();
+            long n = sc.nextLong();
+            long sheets = 1;
+            while (w % 2 == 0) {
+                w = w / 2;
+                sheets = sheets * 2;
+            }
+            while (h % 2 == 0) {
+                h = h / 2;
+                sheets = sheets * 2;
+            }
+            if (sheets >= n) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+
+    }
+}
