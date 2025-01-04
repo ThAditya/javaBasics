ccd7324b96fb8851cbf117b0e2cf3ccecd63beab
ThAditya <aditya.vashishth95@gmail.com>
2025-01-05 00:21:16 +0530

    pairs in Array

---
+++ pairsInArray.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class pairsInArray {
+    public static void pairs(int nums[]){
+        for(int i=0;i<nums.length;i++){
+            int curr = nums[i];
+            for(int j=i+1;j<nums.length;j++){
+                System.out.print( "(" + curr + ',' + nums[j]+ ")");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("length:");
+        int n = sc.nextInt();
+        System.out.println("Elements: ");
+        int nums[] = new int[n];
+
+        for(int i=0;i<n;i++){
+            nums[i] = sc.nextInt();
+        }
+
+        pairs(nums);
+    }
+}
