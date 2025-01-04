7187ec649fe7abc0696d7e74caf1ea962501d18e
Aditya Vashishth <156231205+ThAditya@users.noreply.github.com>
2025-01-05 01:22:30 +0530

    Update SubArrays.java

--- SubArrays.java
+++ SubArrays.java
@@ -2,21 +2,28 @@ import java.util.*;
 public class SubArrays {
 
     public static void printSubArrays(int num[]){
-        int tp = 0;
+        int tp = 0, sum =0, min = Integer.MAX_VALUE, max =Integer.MIN_VALUE;
         for(int i=0;i<num.length;i++){
             int start = i;
             for(int j=i;j<num.length;j++){
                 int end = j;
                 for(int k=start;k<=end;k++){
                     System.out.print(num[k]);
-                    
+                    sum = sum + num[k];
                 }
                 System.out.println();
                 tp++;
+                
             }
+            System.out.println("sum of SubArray is: "  + sum);
             System.out.println();
+            if (min > sum) min = sum;
+            if (max < sum) max = sum;
+            sum = 0;
         }
-        System.out.println("Total number of subarrays: "+tp);
+
+
+        System.out.println("Total number of SubArrays: "+tp + ", Maximum: " + max + ", Minimum: " + min );
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
