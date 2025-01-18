44c114056e6b65bc44d1517144722b76de497ed6
ThAditya <aditya.vashishth95@gmail.com>
2025-01-18 23:04:32 +0530

    Counting Sort

---
+++ Sorting/countingSort.java
@@ -0,0 +1,43 @@
+package Sorting;
+
+import java.util.Scanner;
+
+public class countingSort {
+
+    public static void printCountingSort(int[] arr){
+        int largest = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            largest = Math.max(largest, arr[i]);
+        }
+
+        int count[] = new int[largest+1];
+        for (int i = 0; i < arr.length; i++) {
+            count[arr[i]]++;
+        }
+
+        int j=0;
+        for(int i=0;i<count.length;i++){
+            while (count[i]>0) {
+                arr[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + "");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        printCountingSort(arr);
+    }
+}
