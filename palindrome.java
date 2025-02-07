9d81f848ab0dcec5ee70111f9a396497b9d81e00
Aditya Vashishth <156231205+ThAditya@users.noreply.github.com>
2025-02-08 00:29:50 +0530

    update

--- palindrome.java
+++ palindrome.java
@@ -1,8 +1,7 @@
 import java.util.*;
 public class palindrome {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+
+    public static boolean palindromeOrNot(int n){
         int temp = n, ret = 0;
         while(n!= 0){
             int rev = n%10;
@@ -10,10 +9,15 @@ public class palindrome {
             n = n/10;
         }
         if(temp == ret){
-            System.out.println("True");
-        }else{
-            System.out.println("False");
+            return true;
         }
+        return false;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        System.out.println(palindromeOrNot(n));
         
     }
 }
