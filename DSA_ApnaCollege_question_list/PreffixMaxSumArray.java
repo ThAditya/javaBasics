35a4ef284bc19411c470844399e0446d90517c75
Aditya Vashishth <156231205+ThAditya@users.noreply.github.com>
2025-03-30 21:41:11 +0530

    new java codes

--- DSA_ApnaCollege_question_list/PreffixMaxSumArray.java
+++ DSA_ApnaCollege_question_list/PreffixMaxSumArray.java
@@ -6,24 +6,28 @@ public class PreffixMaxSumArray {
 
     public static int printPrefixSum(int[] arr) {
         
-        int max = Integer.MIN_VALUE, sum =0;
+        int max = Integer.MIN_VALUE, sum = 0;
         int prefixSum[] = new int[arr.length];
         prefixSum[0] = arr[0];
 
-        for(int i=1;i<arr.length;i++){
-            prefixSum[i] = prefixSum[i-1] + arr[i];
+        for(int i=1;i<prefixSum.length;i++){
+            prefixSum[i] = prefixSum[i-1] + arr[i];     
         }
-
+        
         for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr.length;j++){
-                
-                sum = i == 0 ? prefixSum[j] : prefixSum[j]- prefixSum[i-1];
+            for(int j=i;j<arr.length;j++){
+                if(i == 0){
+                    sum = prefixSum[j];
+                }else{
+                    sum = prefixSum[j] - prefixSum[i-1];
+                }
 
                 if(max < sum) max = sum;
-
             }
-        }        
+        }
         return max;
+    
+        
     }
     public static void main(String[] args) {
         Scanner sc  = new Scanner(System.in);
