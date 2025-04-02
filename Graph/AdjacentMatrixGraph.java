fe9a5e6e8b357628664ff7e570a52f4a0afd12ec
Aditya Vashishth <156231205+ThAditya@users.noreply.github.com>
2025-04-02 22:33:30 +0530

    Updation

--- Graph/AdjacentMatrixGraph.java
+++ Graph/AdjacentMatrixGraph.java
@@ -31,10 +31,8 @@ public class AdjacentMatrixGraph {
     
     public static void main(String[] args) {
         AdjacentMatrixGraph g = new AdjacentMatrixGraph(4);
-        g.addEdge(0, 1);
-        g.addEdge(1, 2);
-        g.addEdge(2, 3);
-        g.addEdge(3, 0);
+        g.addEdge(0, 1);g.addEdge(1, 2);
+        g.addEdge(2, 3);g.addEdge(3, 0);
         System.out.println(g);
     }
 }
