e3cf2b67bc2bc11dc7e0c0f14d298e882126c189
Aditya Vashishth <156231205+ThAditya@users.noreply.github.com>
2025-06-26 00:30:40 +0530

    LinkedLIst

--- tripletSum.java
+++ tripletSum.java
@@ -1,39 +1,39 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
+// import java.util.ArrayList;
+// import java.util.Collections;
+// import java.util.List;
+// import java.util.Scanner;
 
-public class tripletSum {
-    public List<List<Integer>> threeSum(int nums[]){
-        List<List<Integer>> result =  new    ArrayList <List<Integer>> ();
-         for(int i=0; i<nums.length; i++) { 
-            for(int j=i+1; j<nums.length; j++) { 
-                for(int k=j+1; k<nums.length; k++) { 
-                    if(nums[i] +  nums[j] +  nums[k] ==   0) 
+// public class tripletSum {
+//     public List<List<Integer>> threeSum(int nums[]){
+//         List<List<Integer>> result =  new    ArrayList <List<Integer>> ();
+//          for(int i=0; i<nums.length; i++) { 
+//             for(int j=i+1; j<nums.length; j++) { 
+//                 for(int k=j+1; k<nums.length; k++) { 
+//                     if(nums[i] +  nums[j] +  nums[k] ==   0) 
 
-                        List<Integer> triplet = new ArrayList<Integer>();
-                        triplet.add(nums[i]);
-                        triplet.add(nums[j]);triplet.add(nums[k]);
-                        Collections.sort(triplet);
-                        result.add(triplet);
-                    }
-                }
-            }
-        }
-        result=new ArrayList<List<Integer>>(new LinkedHashSet<List<Integer>>(result));
-    return result;
-}
+//                         List<Integer> triplet = new ArrayList<Integer>();
+//                         triplet.add(nums[i]);
+//                         triplet.add(nums[j]);triplet.add(nums[k]);
+//                         Collections.sort(triplet);
+//                         result.add(triplet);
+//                     }
+//                 }
+//             }
+//         }
+//         result=new ArrayList<List<Integer>>(new LinkedHashSet<List<Integer>>(result));
+//     return result;
+// }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
 
-        int arr[] = new int[n];
+//         int arr[] = new int[n];
 
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
+//         for (int i = 0; i < n; i++) {
+//             arr[i] = sc.nextInt();
+//         }
 
-        printTripletOutput(arr);
-    }
-}
+//         printTripletOutput(arr);
+//     }
+// }
