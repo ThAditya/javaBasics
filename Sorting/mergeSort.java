208b432a83b6acec0f5b2cc787faed614efa4d63
Aditya Vashishth <156231205+ThAditya@users.noreply.github.com>
2025-12-27 22:07:18 +0530

    Merge Sort using Recursion

--- Sorting/mergeSort.java
+++ Sorting/mergeSort.java
@@ -4,8 +4,15 @@ import java.util.Scanner;
 
 public class mergeSort {
 
+    public static void printArr(int arr[]){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println(); 
+    }
+
     public static void printMergeArr(int arr[], int si, int ei){
-        if(si == ei || si > ei){
+        if(si >= ei){
             return;
         }
         int mid = si + (ei - si)/2;
@@ -23,7 +30,7 @@ public class mergeSort {
         int k = 0;
 
         while(i <= mid && j <= ei){
-            if(arr[i]>arr[j]){
+            if(arr[i]<arr[j]){
                 temp[k] = arr[i];
                 i++;k++;
             }else{
@@ -31,16 +38,35 @@ public class mergeSort {
                 j++;k++;
             }
         }
+
+        // for left
+        while(i <= mid){
+            temp[k++] = arr[i++];
+        }
+        
+        // for right
+        while(j <= ei){
+            temp[k++] = arr[j++];
+        }
+
+        //  copy temp to original array
+
+        for(k=0, i= si;k<temp.length;k++, i++){
+            arr[i] = temp[k];
+        }
+ 
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int arr[] = new int[n];
-        for (int i = 0; i < arr.length; i++) {
+        for(int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
         }
-        int si = arr[0];
-        int ei = arr[arr.length-1];
+        int si = 0;
+        int ei = arr.length-1;
+          
         printMergeArr(arr,si,ei);
+        printArr(arr);
     }
 }
