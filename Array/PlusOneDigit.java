940ffcaa0eaa5ec3fb02ea0f23124a8808c124d3
Aditya Vashishth <156231205+ThAditya@users.noreply.github.com>
2026-01-03 22:53:36 +0530

    Plus One at the end of array

--- Array/PlusOneDigit.java
+++ Array/PlusOneDigit.java
@@ -1,10 +1,12 @@
 package Array;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class PlusOneDigit {
 
-    public static int[] plusOne(int []arr, int n){
+    public static int[] plusOne(int []arr){
+        int n = arr.length;
         for(int i=n-1;i>=0;i--){
             if(arr[i] < 9){
                 arr[i]++;
@@ -19,12 +21,13 @@ public class PlusOneDigit {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        PlusOneDigit PLD = new PlusOneDigit();
         int n = sc.nextInt();
         int[] arr = new int[n];
         for(int i=0;i<n;i++){
             arr[i] = sc.nextInt();
         }
-        
-        System.out.println(plusOne(arr, n));
+        int[] result = PLD.plusOne(arr);
+        System.out.println(Arrays.toString(result));
     }
 }
