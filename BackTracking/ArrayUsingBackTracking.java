04e62cb13950043e8a3eea124479dc2d33376adc
Aditya Vashishth <156231205+ThAditya@users.noreply.github.com>
2026-01-05 22:50:14 +0530

    Array using Backtracking

---
+++ BackTracking/ArrayUsingBackTracking.java
@@ -0,0 +1,32 @@
+package BackTracking;
+
+import java.util.Scanner;
+
+public class ArrayUsingBackTracking {
+
+    public static void changeArray(int[] arr, int i, int val){
+        if(i == arr.length){
+            printArray(arr);
+            return;
+        }
+        arr[i] = val;
+        changeArray(arr, i+1, val+1);
+        arr[i] = arr[i] - 2;
+
+    }
+
+    public static void printArray(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        changeArray(arr, 0, 1);
+        printArray(arr);
+
+    }
+}
